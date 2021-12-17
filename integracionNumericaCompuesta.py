94dd3ab0d2851a9d3e1e0eef946afdd4825e9c59
Emanuel <emanuel.palestino.h@gmail.com>
2021-12-17 16:36:09 -0600

    cambios

--- integracionNumericaCompuesta.py
+++ integracionNumericaCompuesta.py
@@ -50,19 +50,21 @@ def reglaCompuestaSimpson(func, a, b, n):
 
 # Funciones de prueba
 def funcion1(x):
-	return 
+	return exp(x)
 
 def funcion2(x):
-	return sqrt(1 + x ** 2)
+	return exp(x ** 2)
 
 def funcion3(x):
-	return 1
+	return x
 
 # Prueba
 print("Integracion Regla Compuesta del Trapecio\n")
-print("Integral 1", reglaCompuestaTrapecio(funcion1, 0, 2, 1000))
-print("Integral 2", reglaCompuestaTrapecio(funcion2, 0, 2, 1000))
+print("Integral 1", reglaCompuestaTrapecio(funcion1, 0, 5, 1000))
+print("Integral 2", reglaCompuestaTrapecio(funcion2, 0, 1, 1000))
+print("Integral 2", reglaCompuestaTrapecio(funcion2, -5, 5, 1000))
 
 print("Integracion Regla Compuesta de Simpson\n")
-print("Integral 1", reglaCompuestaTrapecio(funcion1, 0, 2, 1000))
-print("Integral 2", reglaCompuestaTrapecio(funcion2, 0, 2, 1000))
+print("Integral 1", reglaCompuestaSimpson(funcion1, 0, 5, 1000))
+print("Integral 2", reglaCompuestaSimpson(funcion2, 0, 1, 1000))
+print("Integral 2", reglaCompuestaSimpson(funcion2, -5, 5, 1000))
